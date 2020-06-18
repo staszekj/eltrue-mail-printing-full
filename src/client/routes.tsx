91a97c05a2dfc14a3523367fbe6cdb16a9cad800
Stanislaw Jagiella <Stanislaw.Jagiella@gmail.com>
2020-06-18 07:37:14 +0200

    + replacing redux by context provider with hooks

--- src/client/routes.tsx
+++ src/client/routes.tsx
@@ -1,25 +1,35 @@
-import { Route, Switch } from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import App from "./view/App";
-import React, { FunctionComponent, useEffect } from "react";
-import { appInitAction } from "./view/app-actions";
-import { useDispatch } from "react-redux";
+import React, {FunctionComponent, useContext, useEffect} from "react";
+import {appInitAction} from "./view/app-actions";
+import {useDispatch} from "react-redux";
 import {PrintedMails} from './view/printed-mails/printed-mails'
+import {AppContext} from "./contexts/app-context";
 
 export const AppRoutes: FunctionComponent = () => {
 
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(appInitAction());
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    const dispatch = useDispatch();
+    const appContext = useContext(AppContext);
+    useEffect(() => {
+        if (!appContext) {
+            return
+        }
+        appContext.printedMails.api.downloadMails();
+        dispatch(appInitAction());
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return (
-    <Switch>
-      <Route path="/printed-mails">
-        <PrintedMails />
-      </Route>
-      <Route exact={true} path="/">
-        <App />
-      </Route>
-    </Switch>
-  );
+    if (!appContext) {
+        return null
+    }
+
+    return (
+        <Switch>
+            <Route path="/printed-mails">
+                <PrintedMails printedMailsApi={appContext.printedMails}/>
+            </Route>
+            <Route exact={true} path="/">
+                <App/>
+            </Route>
+        </Switch>
+    );
 };
--- src/client/view/printed-mails/printed-mails.tsx
+++ src/client/view/printed-mails/printed-mails.tsx
@@ -10,8 +10,8 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { TThemeReducer } from "../../redux/theme-reducer";
-import { getTheme } from "../../view/app-selectors";
+import {TThemeReducer} from "../../redux/theme-reducer";
+import {TPrintedMailsApi} from "../../hooks/printed-mails-hook";
 
 const useStyles = makeStyles((theme: TThemeReducer) => {
   return {
@@ -45,21 +45,23 @@ const useStyles = makeStyles((theme: TThemeReducer) => {
   }
 });
 
+export type TPrintedMailsProps = {
+  printedMailsApi: TPrintedMailsApi
+}
 
-export const PrintedMails: FunctionComponent<{}> = () => {
+export const PrintedMails: FunctionComponent<TPrintedMailsProps> = ({printedMailsApi}) => {
+  const classes = useStyles();
+  const rows = printedMailsApi.response;
 
-  const rows = useSelector(getMails);
-  const theme = useSelector(getTheme);
-  const classes = useStyles(theme);
 
   return (
       <div className={classes.MainVertical}>
-          <div className={classes.Header}>
-              {"//TODO toolbar"}
-          </div>
-          <div className={classes.Main}>
-            <TableContainer className={classes.ScrollContainer} component={Paper}>
-              <Table className={classes.table} size="small" aria-label="a dense table">
+        <div className={classes.Header}>
+          {"//TODO toolbar"}
+        </div>
+        <div className={classes.Main}>
+          <TableContainer className={classes.ScrollContainer} component={Paper}>
+            <Table className={classes.table} size="small" aria-label="a dense table">
                 <TableHead>
                   <TableRow>
                     <TableCell>timeStamp</TableCell>
@@ -73,17 +75,17 @@ export const PrintedMails: FunctionComponent<{}> = () => {
                 </TableHead>
                 <TableBody>
                   {rows.map((row) => (
-                    <TableRow key={row.attachmentId}>
-                      <TableCell component="th" scope="row">
-                        {row.timeStamp}
-                      </TableCell>
-                      <TableCell align="right">{row.pagesRanges}</TableCell>
-                      <TableCell align="right">{row.reason}</TableCell>
-                      <TableCell align="right">{row.sentDateMmtUtc}</TableCell>
-                      <TableCell align="right">{row.from}</TableCell>
-                      <TableCell align="right">{row.subject}</TableCell>
-                      <TableCell align="right">{row.fileName}</TableCell>
-                    </TableRow>
+                      <TableRow key={row.messageId}>
+                        <TableCell component="th" scope="row">
+                          {row.timeStamp}
+                        </TableCell>
+                        <TableCell align="right">{row.pagesRanges}</TableCell>
+                        <TableCell align="right">{row.reason}</TableCell>
+                        <TableCell align="right">{row.sentDateMmtUtc}</TableCell>
+                        <TableCell align="right">{row.from}</TableCell>
+                        <TableCell align="right">{row.subject}</TableCell>
+                        <TableCell align="right">{row.fileName}</TableCell>
+                      </TableRow>
                   ))}
                 </TableBody>
               </Table>
